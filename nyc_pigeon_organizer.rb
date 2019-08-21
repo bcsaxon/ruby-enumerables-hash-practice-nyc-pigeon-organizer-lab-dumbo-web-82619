b51139f2625cf0575d53277b4f9d69183725efe7
Brett Saxon <brettcsaxon@gmail.com>
2019-08-21 18:39:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,20 @@
-def nyc_pigeon_organizer(data)
-  def nyc_pigeon_organizer(pigeon_data)
+def nyc_pigeon_organizer(pigeon_data)
     pigeon_list = {}
 
-    pigeon_data.each do |property, property_hash| #access :color and colors-hash
-     property_hash.each do |values, pigeon_array| #access colors-hash and array value
-      pigeon_array.each do |name| #access each pigeon name in array
-        if pigeon_list[name] #if name in list, do next line; else -> line 14
-          if pigeon_list[name][property] #if property in list, do next line, else line 11
-            pigeon_list[name][property] << values.to_s #adds value of property to name
+    pigeon_data.each do |property, property_hash|
+     property_hash.each do |values, pigeon_array|
+      pigeon_array.each do |name|
+        if pigeon_list[name]
+          if pigeon_list[name][property]
+            pigeon_list[name][property] << values.to_s
           else
-            pigeon_list[name][property] = [values.to_s] #adds property/value-array to hash
+            pigeon_list[name][property] = [values.to_s]
           end
         else
-          pigeon_list[name] = { property => [values.to_s] } #adds name hash and key/value (property/values)
+          pigeon_list[name] = { property => [values.to_s] }
         end
       end
     end
   end
     pigeon_list
   end
-end
